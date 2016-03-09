59785451211c3376f13f9adadc359d29f81ead16
Orlando Hoilett <orlandohoilett@pal-nat186-15-64.itap.purdue.edu>
2016-03-08 20:20:02 -0500

    class declaration fiile for Xylophone class

---
+++ Xylophone.h
@@ -0,0 +1,14 @@
+//
+//  Xylophone.h
+//  
+//
+//  Created by Orlando Hoilett on 3/8/16.
+//
+//
+
+#ifndef ____Xylophone__
+#define ____Xylophone__
+
+#include <stdio.h>
+
+#endif /* defined(____Xylophone__) */
